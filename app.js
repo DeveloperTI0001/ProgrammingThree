869603a76d8df29e92b2c0c3a150abd84b2147b1
Carlos <carlos@vbox.myguest.virtualbox.org>
2025-11-04 20:39:49 -0500

    test

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('My App')
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   res.json('Response') 
+});
+
+const PORT = 4000;
+app.listen(PORT, (req, res) => {
+    console.log(`Online http://localhost:${PORT}`)
+})
